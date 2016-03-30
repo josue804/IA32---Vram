3acc3fb5119d5f1eb86f05081eb84564859769d5
josue804 <josuevalverde12@gmail.com>
2016-03-30 12:58:00 -0700

    set-up initial files for vram assembly IA32 simulation

---
+++ video1.c
@@ -0,0 +1,11 @@
+#include "video.h"
+
+int main(int argc, char** argv) {
+    int i, j;
+    cls();
+    video[2][38][0] = 'h';  video[2][40][0] = 'i';  video[2][42][0] = '!';
+    video[2][38][1] = 0x51; video[2][40][1] = 0x43; video[2][42][1] = 0x35;
+    display();
+    return 0;
+}
+
